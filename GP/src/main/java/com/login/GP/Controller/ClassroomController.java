8d08386e8bb55a3ae2332ffb59d1b5777d7c6be0
Mohamed Adel <mohamedadel153707@gmail.com>
2018-12-06 23:45:09 +0200

    task1-classrooms&it's api
    s

--- GP/src/main/java/com/login/GP/Controller/ClassroomController.java
+++ GP/src/main/java/com/login/GP/Controller/ClassroomController.java
@@ -1,12 +1,50 @@
 package com.login.GP.Controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import com.login.GP.Model.ClassRoom;
 
-import com.login.GP.Repository.ClassroomRepository;
+import com.login.GP.Repository.ClassRoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
+
+import java.util.Optional;
+
 
 @RestController
-public class ClassroomController {
-	@Autowired
-	ClassroomRepository classroomRepo;
+public class ClassRoomController {
+
+    @Autowired
+    ClassRoomRepository classRoomService;
+
+    @PostMapping("/classrooms")
+    ClassRoom create(@Valid @RequestBody ClassRoom classroom){
+        return classRoomService.save(classroom);
+    }
+
+
+    @GetMapping("/classrooms")
+    Iterable<ClassRoom> read(){
+        return classRoomService.findAll();
+    }
+
+    @PutMapping("/classrooms")
+    ResponseEntity<ClassRoom> update(@RequestBody ClassRoom classRoom){
+        if(classRoomService.findById(classRoom.getClassId()).isPresent())
+            return new ResponseEntity(classRoomService.save(classRoom), HttpStatus.OK);
+        else
+            return new ResponseEntity(classRoom,HttpStatus.BAD_REQUEST);
+    }
+
+    @DeleteMapping("/classrooms/{id}")
+    void delete(@PathVariable Integer id){
+        classRoomService.deleteById(id);
+    }
+
+    @GetMapping("/classrooms/{id}")
+    Optional<ClassRoom> findById(@PathVariable Integer id){
+        return classRoomService.findById(id);
+    }
+
 }
--- GP/src/main/java/com/login/GP/Model/Classroom.java
+++ GP/src/main/java/com/login/GP/Model/Classroom.java
@@ -1,11 +1,6 @@
 package com.login.GP.Model;
 
-import org.springframework.data.annotation.Id;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
