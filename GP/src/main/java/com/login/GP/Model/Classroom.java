c1382886e631110a424267df5ec41912147147f9
Mohamed Adel Tawfiq <mohamedadel153707@gmail.com>
2018-12-06 16:37:26 +0200

    Update Classroom.java

--- GP/src/main/java/com/login/GP/Model/Classroom.java
+++ GP/src/main/java/com/login/GP/Model/Classroom.java
@@ -1,53 +1,80 @@
 package com.login.GP.Model;
 
+import org.springframework.data.annotation.Id;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
-public class Classroom {
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	
-private int classID;
-private int creatorID;
-private int classType;
-private String category;
-private String PassCode ;
+@Table(name = "classroom")
+public class ClassRoom {
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private int classId;
+
+    @NotNull
+    private int creatorId;
+
+    private int classType;
+
+    @NotBlank
+    @Size (max = 255)
+    private String category;
+
+    @NotBlank
+    @Size (max = 255)
+    private String passCode;
 
 
+    public ClassRoom(@NotNull int creatorId, int classType, @NotBlank @Size(max = 255) String category, @NotBlank @Size(max = 255) String passCode) {
+        this.creatorId = creatorId;
+        this.classType = classType;
+        this.category = category;
+        this.passCode = passCode;
+    }
 
-public int getClassID() {
-	return classID;
-}
-public void setClassID(int classID) {
-	this.classID = classID;
-}
-public int getCreatorID() {
-	return creatorID;
-}
-public void setCreatorID(int creatorID) {
-	this.creatorID = creatorID;
-}
-public int getClassType() {
-	return classType;
-}
-public void setClassType(int classType) {
-	this.classType = classType;
-}
-public String getCategory() {
-	return category;
-}
-public void setCategory(String category) {
-	this.category = category;
-}
-public String getPassCode() {
-	return PassCode;
-}
-public void setPassCode(String passCode) {
-	PassCode = passCode;
-}
-	
-	
+    public int getClassId() {
+        return classId;
+    }
+
+    public void setClassId(int classId) {
+        this.classId = classId;
+    }
+
+    public int getCreatorId() {
+        return creatorId;
+    }
+
+    public void setCreatorId(int creatorId) {
+        this.creatorId = creatorId;
+    }
+
+    public int getClassType() {
+        return classType;
+    }
+
+    public void setClassType(int classType) {
+        this.classType = classType;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getPassCode() {
+        return passCode;
+    }
+
+    public void setPassCode(String passCode) {
+        this.passCode = passCode;
+    }
 }
