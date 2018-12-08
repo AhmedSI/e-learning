6ffcb24c5f66403bfaa44b2697e54b3e589b5cb7
Mohamed Atef <34000758+mohamedatef22@users.noreply.github.com>
2018-12-08 16:11:26 +0200

    Update Mapping.java

--- Back_End/src/main/java/com/adaptivelearning/server/constants/Mapping.java
+++ Back_End/src/main/java/com/adaptivelearning/server/constants/Mapping.java
@@ -3,9 +3,6 @@ package com.adaptivelearning.server.constants;
 public class Mapping {
     public static final String LOGIN = "/login";
     public static final String REGISTER = "/register";
-    public static final String BASE_AUTH = "/api/auth";    
-    public static final String Class_Rooms = "/classrooms";
-    public static final String Class_Rooms_Id = "/classrooms/{id}"; //search or delete by id
-    public static final String Class_Rooms_Creator_Id = "/classrooms/creator/{id}"; //return all classrooms for creator id
+    public static final String BASE_AUTH = "/api/auth";
 
 }
