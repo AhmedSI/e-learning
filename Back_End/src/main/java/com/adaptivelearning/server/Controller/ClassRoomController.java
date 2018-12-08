9c3835b50ec62289db36854d9319730cb3422d4e
Mohamed Atef <34000758+mohamedatef22@users.noreply.github.com>
2018-12-08 16:12:08 +0200

    Update ClassRoomController.java

--- Back_End/src/main/java/com/adaptivelearning/server/Controller/ClassRoomController.java
+++ Back_End/src/main/java/com/adaptivelearning/server/Controller/ClassRoomController.java
@@ -4,12 +4,17 @@ import com.adaptivelearning.server.Model.ClassRoom;
 import com.adaptivelearning.server.Repository.ClassRoomRepository;
 import com.adaptivelearning.server.Repository.UserRepository;
 
-import com.adaptivelearning.server.constants.Mapping;
-import com.adaptivelearning.server.payload.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
@@ -25,39 +30,31 @@ public class ClassRoomController {
     @Autowired
     UserRepository userRepository;
 
-    @PostMapping(Mapping.Class_Rooms)
-    ResponseEntity<?> create(@Valid @RequestBody ClassRoom classroom){
-        classRoomService.save(classroom);
-        return ResponseEntity.ok().body(new ApiResponse(200, "Classroom created successfully"));
+    @PostMapping("/classrooms")
+    ResponseEntity<ClassRoom> create(@Valid @RequestBody ClassRoom classroom){
+        return new ResponseEntity(classRoomService.save(classroom),HttpStatus.OK);
     }
-    @PutMapping(Mapping.Class_Rooms)
-    ResponseEntity<?> update(@RequestBody ClassRoom classRoom){
+    @PutMapping("/classrooms")
+    ResponseEntity<ClassRoom> update(@RequestBody ClassRoom classRoom){
         if(classRoomService.findById(classRoom.getClassId()).isPresent())
-        {
-            classRoomService.save(classRoom);
-            return ResponseEntity.ok().body(new ApiResponse(200, "Classroom updated successfully"));
-        }
+            return new ResponseEntity(classRoomService.save(classRoom), HttpStatus.OK);
         else
-        {
-            return new ResponseEntity(new ApiResponse(500, "Classroom dos not exist"),
-                    HttpStatus.BAD_REQUEST);
-        }
+            return new ResponseEntity(classRoom,HttpStatus.BAD_REQUEST);
     }
-    @GetMapping(Mapping.Class_Rooms)
+    @GetMapping("/classrooms")
     Iterable<ClassRoom> read(){
         return classRoomService.findAll();
     }
 
-    @DeleteMapping(Mapping.Class_Rooms_Id)
+    @DeleteMapping("/classrooms/{id}")
     void delete(@PathVariable Integer id){
         classRoomService.deleteById(id);
     }
-    @GetMapping(Mapping.Class_Rooms_Id)
+    @GetMapping("/classrooms/{id}")
     Optional<ClassRoom> findById(@PathVariable Integer id){
         return classRoomService.findById(id);
     }
-    //return all classrooms for creator id
-    @GetMapping(Mapping.Class_Rooms_Creator_Id)
+    @GetMapping("/classrooms/creator/{id}")
     Iterable<ClassRoom> findByQueryCreator(@PathVariable Integer id){
         return classRoomService.findByCreatorId(id);
     }
